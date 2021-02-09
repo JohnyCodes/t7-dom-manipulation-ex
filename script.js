3ae9e0eaeacdcc3920b30b7fb45a307666207757
Ioannis Apanozidis <ioan0673@stud.kea.dk>
2021-02-09 10:56:16 +0100

    Solution

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //Write your own code here
 //DON'T touch any other files!
+const allBr = document.querySelectorAll("br");
+allBr.forEach((brEl) => {
+  brEl.remove();
+});
+
+const allSections = document.querySelectorAll("section");
+allSections.forEach((secEl) => {
+  secEl.classList.add("filled");
+  secEl.previousElementSibling.remove();
+});
+
+// const allSections = document.querySelectorAll("section");
+// allSections.forEach((secEl) => {
+//   secEl.classList.add("filled");
+// });
+
+// const allDivs = document.querySelectorAll("div");
+// console.log(allDivs);
+// // allDivs.forEach((divEl) => {
+// //   divEl.remove();
+// // });
